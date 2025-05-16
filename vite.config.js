6a2f1093a7e5479c7ebe24bf55c7bb1262ca8986
amatti-12 <147675650+amatti-12@users.noreply.github.com>
2025-05-16 17:09:07 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Retreat-App/', // ✅ This must match the GitHub repo name exactly
   plugins: [react()],
-})
+});
